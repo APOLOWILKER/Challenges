f249830210fa073b2b351b6b479debc44eeecdbe
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-06-12 11:07:34 -0300

    casting de cachorro para Animal.

---
+++ src/animal/Test.java
@@ -0,0 +1,8 @@
+package animal;
+
+public class Test {
+    public static void main(String[] args) {
+        Cachorro cachorro = new Cachorro();
+        Animal animal = (Animal) cachorro;
+    }
+}
