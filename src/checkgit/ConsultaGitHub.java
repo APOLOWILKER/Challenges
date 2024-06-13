8feaf2f26bf11e1a2c060949fbf7678cbd3d3a32
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-06-13 10:56:03 -0300

    check githubApi and create handler exception

---
+++ src/checkgit/ConsultaGitHub.java
@@ -0,0 +1,41 @@
+package checkgit;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class ConsultaGitHub {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite o nome de usuário do GitHub");
+        String username = leitura.nextLine();
+
+        String endereco = "https://api.github.com/users/" + username;
+
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(endereco))
+                    .header("Accept", "application/vnd.github.v3+json")
+                    .build();
+
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            if(response.statusCode() == 404) {
+                throw new ErrorConsultaGitHubException("Usuário não encontrado no GitHub.");
+            }
+
+            String json = response.body();
+            System.out.println(json);
+
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Opss... Houve um erro durante a requisição");
+        } catch (ErrorConsultaGitHubException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ src/checkgit/ErrorConsultaGitHubException.java
@@ -0,0 +1,7 @@
+package checkgit;
+
+public class ErrorConsultaGitHubException extends RuntimeException {
+    public ErrorConsultaGitHubException(String mensagem){
+        super(mensagem);
+    }
+}
